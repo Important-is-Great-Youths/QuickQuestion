b4045248a76d2c7481dcb9b019f8b50f3ad69d48
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-04 16:06:11 +0900

    style: 컴포넌트 별 폴더 생성

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind'
+import styles from './Input.module.scss'
+import { Controller, RegisterOptions, useFormContext } from 'react-hook-form'
+
+const cx = classNames.bind(styles)
+
+interface InputProps {
+  size: 'lg' | 'sm'
+  type: 'text' | 'password'
+}
+
+const Input = ({ size, type }: InputProps) => {
+  return <input className={cx('input', `input-size-${size}`)} type={type} />
+}
+
+export default Input
