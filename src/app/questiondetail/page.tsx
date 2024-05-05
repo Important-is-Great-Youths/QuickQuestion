689a18cd0e6ec54b2ea8691b5e26ce88c84e3e84
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-06 01:04:31 +0900

    design: questiondetail 페이지 파일 생성

---
+++ src/app/questiondetail/page.tsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind'
+import styles from './questiondetail.module.scss'
+
+const cx = classNames.bind(styles)
+
+const QuestionDetailPage = () => {
+  return <div>page</div>
+}
+
+export default QuestionDetailPage
