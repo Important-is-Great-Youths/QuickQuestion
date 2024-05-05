b3a857a95992e99f2e79a719273fe935bfc9fbfb
mini-chip <kimmin5209@gmail.com>
2024-05-05 22:33:20 +0900

    feat: pagination 진행 중 브랜치 이동

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,58 +1,86 @@
 import classNames from "classnames/bind";
 import styles from "./Pagination.module.scss";
+import { useState } from "react";
+import data from "./data.json";
 
 const cx = classNames.bind(styles);
+
 interface PaginationProps {
   size: "lg" | "md";
+  totalCount: number;
+  limit: number;
 }
+//total 데이터 총 갯수, limit 한 페이지에 나타낼 데이터 갯수
+export const Pagination = ({
+  totalCount = data.length, //데이터의 총 갯
+  limit = 6, //한 페이지에 나타낼 데이터 수
+  size
+}: PaginationProps) => {
+  const totalPage = Math.ceil(totalCount / limit);
+  const [currentPage, setCurrentPage] = useState(1); //현재 페이지
+  const [offset, setOffest] = useState(0); //데이터 불러온 갯수
+  const pageCount = 5; //화면에 보여지는 총 페이지 갯수
 
-export const Pagination = ({ size }: PaginationProps) => {
-  // const totalPages = Math.ceil(totalCount / limit)
+  let firstNumber = Math.max(1, currentPage - Math.floor(pageCount / 2));
+  let lastNumber = Math.min(totalPage, firstNumber + pageCount - 1);
 
-  // const handlePageChange = (newPage: number) => {
-  //   if (newPage >= 1 && newPage <= totalPages) {
-  //     onPageChange(newPage)
-  //   }
-  // }
+  const prev = Math.max(1, currentPage - 1); // 이전버튼
+  const next = Math.min(totalPage, currentPage + 1); //다음버튼
+
+  const buttons = []; //버튼들 배열
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    buttons.push(
+      <button
+        key={i}
+        className={cx("button", `button-size-${size}`, {
+          "button-active": i === currentPage
+        })}
+        onClick={() => setCurrentPage(i)}
+      >
+        {i}
+      </button>
+    );
+  }
 
   return (
-    <div className={cx(`pagination`)}>
-      <button
-        type="button"
-        className={cx(`button`, `button-size-${size}`)}
-        // onClick={() => handlePageChange(currentPage - 1)}
-        // disabled={currentPage === 1}
-      >
-        {"<"}
-      </button>
-      <button type="button" className={cx(`button`, `button-size-${size}`)}>
-        {" "}
-        1
-      </button>
-      <button type="button" className={cx(`button`, `button-size-${size}`)}>
-        {" "}
-        2
-      </button>
-      <button type="button" className={cx(`button`, `button-size-${size}`)}>
-        {" "}
-        3
-      </button>
-      <button type="button" className={cx(`button`, `button-size-${size}`)}>
-        {" "}
-        4
-      </button>
-      <button type="button" className={cx(`button`, `button-size-${size}`)}>
-        {" "}
-        5
-      </button>
-      {/* <span className=".pagination-button.currentPage">{currentPage}</span> */}
-      <button
-        type="button"
-        className={cx(`button`, `button-size-${size}`)} // onClick={() => handlePageChange(currentPage + 1)}
-        // disabled={currentPage === totalPages}
-      >
-        {">"}
-      </button>
+    <div>
+      {data.map(({ id, name }) => (
+        <p key={id}>
+          {id} : {name}
+        </p>
+      ))}
+
+      <div className={cx("pagination")}>
+        <button
+          className={cx("button", `button-size-${size}`)}
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(prev)}
+        >
+          &lt;
+        </button>
+        {buttons}
+        {/* {Array.from({ length: lastNumber - firstNumber + 1 }, (_, index) => {
+          const pageNumber = firstNumber + index;
+          return (
+            <button
+              key={pageNumber}
+              className={cx("button", `button-size-${size}`, {
+                "button-active": pageNumber === currentPage
+              })}
+              onClick={() => setCurrentPage(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          );
+        })} */}
+        <button
+          className={cx("button", `button-size-${size}`)}
+          disabled={currentPage === totalPage}
+          onClick={() => setCurrentPage(next)}
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
 };
