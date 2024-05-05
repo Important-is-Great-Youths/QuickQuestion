2668b52ce55097c9e44c6ee5b4a8b434d974e52c
mini-chip <kimmin5209@gmail.com>
2024-05-05 23:27:34 +0900

    feat: formmodal 진행 중

---
+++ src/components/common/FormModal/FormModal.tsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind'
+import styles from './FormModal.module.scss'
+import {button} from '../Butto
+const cx= classNames.bind(styles);
+
+const FormModal =()=>{
+  return(
+
+  )
+}
