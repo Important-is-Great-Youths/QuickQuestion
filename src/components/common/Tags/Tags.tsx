f23e899f744ccfbc5e15e23c14bbb25dbcebe28a
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-06 19:20:20 +0900

    storybook : Tags에 responsive 적용

--- src/components/common/Tags/Tags.tsx
+++ src/components/common/Tags/Tags.tsx
@@ -5,39 +5,38 @@ import { useState } from "react";
 const cx = classNames.bind(styles);
 
 interface TagsProps {
-  size?: "sm" | "md";
-  isAll?: boolean;
-  theme?: "sunny" | "rainy" | "snowy";
+  isAll?: boolean
+  theme?: 'sunny' | 'rainy' | 'snowy'
 }
 
-const tagList: Array<"전체" | "학문" | "연예" | "게임" | "기타"> = [
-  "전체",
-  "학문",
-  "연예",
-  "게임",
-  "기타",
-];
+const tagList: Array<'전체' | '학문' | '연예' | '게임' | '기타'> = [
+  '전체',
+  '학문',
+  '연예',
+  '게임',
+  '기타'
+]
 
-const Tags = ({ size = "md", isAll = true, theme = "sunny" }: TagsProps) => {
-  const filteredTagList = isAll ? tagList : tagList.slice(1);
+const Tags = ({ isAll = true, theme = 'sunny' }: TagsProps) => {
+  const filteredTagList = isAll ? tagList : tagList.slice(1)
   return (
-    <div className={cx("tags", `tags-size-${size}`)}>
+    <div className={cx('tags')}>
       {filteredTagList.map((tag) => (
-        <div className={cx("tag", `tag-size-${size}`)}>
+        <div className={cx('tag')}>
           <input
             id={tag}
             type="radio"
             name="Tags"
             value={tag}
-            className={cx("radio")}
+            className={cx('radio')}
           />
-          <label htmlFor={tag} className={cx("label", `label-size-${size}`)}>
+          <label htmlFor={tag} className={cx('label')}>
             {tag}
           </label>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
 export default Tags;
