a72d0c7d121551fc549d8a67205636bb4f0aa850
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-06 15:06:21 +0900

    storybook : AlertModal 컴포넌트 작성

---
+++ src/stories/AlertModal.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import AlertModal from '@/components/common/AlertModal/AlertModal'
+
+const meta = {
+  title: 'AlertModal',
+  component: AlertModal,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+} satisfies Meta<typeof AlertModal>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {}
+}
