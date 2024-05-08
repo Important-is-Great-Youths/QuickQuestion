eeca39adbc77b05828e538f50e95d43a0651e002
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-08 21:44:43 +0900

    feat : ModalProvider 임시 작성

---
+++ src/contexts/ModalProvider.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react'
+
+type ModalType = {
+  id: string
+  content: ReactNode
+}
+
+interface ModalContextValue {
+  openModal: (content: ReactNode, id: string) => void
+  closeModal: (id: string) => void
+  closeAllModals: () => void
+}
+
+const ModalContext = createContext<ModalContextValue>({
+  openModal: () => {},
+  closeModal: () => {},
+  closeAllModals: () => {}
+})
+
+const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [modals, setModals] = useState<ModalType[]>([])
+
+  const openModal = (content: ReactNode, id: string) => {
+    const newModal = { id, content }
+    setModals((prevModals) => [...prevModals, newModal])
+  }
+
+  const closeModal = (id: string) => {
+    setModals(
+      modals.filter((modal) => {
+        return modal.id !== id
+      })
+    )
+  }
+
+  const closeAllModals = () => {
+    setModals([])
+  }
+}
+
+export const useModal = () => {
+  const context = useContext(ModalContext)
+  if (!context) {
+    throw new Error('반드시 ModalProvider 안에서 사용해야 합니다.')
+  }
+
+  return context
+}
+
+export default ModalProvider
