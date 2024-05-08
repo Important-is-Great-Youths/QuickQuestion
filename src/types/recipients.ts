9e61d9e0242a722f45866d2d7cfe029376bcc2fa
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-08 13:45:18 +0900

    feat: 닉네임 중복 validate 구현

--- src/types/recipients.ts
+++ src/types/recipients.ts
@@ -1,3 +1,15 @@
+export interface GetRecipientsList {
+  id: number
+  backgroundColor: string
+  backgroundImageURL: string | null
+  createdAt: string
+  messageCount: number
+  name: string
+  reactionCount: number
+  recentMessages: string[]
+  topReactions: string[]
+}
+
 export interface PostRecipientsCreate {
   team: string
   name: string
