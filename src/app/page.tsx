fb37bfbb2d52709591ade1f6760df57027c9bc08
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-09 13:36:33 +0900

    feat: QuestionForm 구현 및 모듈화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
 'use client'
 
-import { useForm, SubmitHandler, FieldValues } from 'react-hook-form'
 import classNames from 'classnames/bind'
 
-import {
-  useGetRecipientsList,
-  usePostRecipientsCreate
-} from '@/hooks/useRecipients'
-import { GetRecipientsList } from '@/types/recipients'
-import { ERROR_MESSAGE, PLACEHOLDER } from '@/constants/formMessage'
-import Input from '@/components/common/Input/Input'
+import QuestionForm from '@/components/home/QuestionForm/QuestionForm'
 
 import styles from './page.module.scss'
 
 const cx = classNames.bind(styles)
 
 export default function Home() {
-  const { data } = useGetRecipientsList(100)
-  const { mutate } = usePostRecipientsCreate()
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm()
-
-  const onSubmit: SubmitHandler<FieldValues> = (value: FieldValues) => {
-    value.name = `${value.nickName}/${value.password}/${value.question}`
-
-    const newValue = { ...value, ...{ team: '2-3' } }
-  }
-
   return (
     <div className={styles.main}>
       <div className={cx('container')}>
@@ -40,30 +18,7 @@ export default function Home() {
         </div>
         <div>
           <p className={cx('text')}>빠르게 질문 해보세요!</p>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label htmlFor="nickName">닉네임</label>
-              <Input
-                id="nickName"
-                size="lg"
-                type="text"
-                placeholder={PLACEHOLDER.nickname}
-                {...register('nickName', {
-                  required: ERROR_MESSAGE.nickname.required,
-                  validate: (value) =>
-                    data.results.some(
-                      (user: GetRecipientsList) =>
-                        user.name.split('/')[0] === value
-                    )
-                      ? ERROR_MESSAGE.nickname.duplicate
-                      : true
-                })}
-              />
-              {errors.nickName && (
-                <span>{errors.nickName.message?.toString()}</span>
-              )}
-            </div>
-          </form>
+          <QuestionForm />
         </div>
       </div>
     </div>
