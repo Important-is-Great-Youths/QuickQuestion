1c6173fb8d01b62c88d7a2cdf942f21b64223249
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-09 13:55:51 +0900

    story : PwPopUp에 react-hook-form 적용

--- src/components/common/PopUp/PwPopUp.tsx
+++ src/components/common/PopUp/PwPopUp.tsx
@@ -3,6 +3,7 @@ import styles from './PwPopUp.module.scss'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import Input from '../Input/Input'
 import Button from '../Button/Button'
+import { ERROR_MESSAGE } from '@/constants/formMessage'
 
 const cx = classNames.bind(styles)
 
@@ -11,6 +12,8 @@ interface PwPopUpProps {}
 type PwPopUpFormValues = {
   password: string
 }
+
+const pwmessage = ERROR_MESSAGE.password
 
 const PwPopUp = ({}: PwPopUpProps) => {
   const {
@@ -28,10 +31,23 @@ const PwPopUp = ({}: PwPopUpProps) => {
       <p className={cx('pwpopup-label')}>비밀번호</p>
       <div className={cx('pwpopup-inputbox')}>
         <div className={cx('pwpopup-inputbox-input')}>
-          <Input size="sm" type="password" placeholder="****" />
+          <Input
+            size="sm"
+            type="password"
+            placeholder="****"
+            {...register('password', {
+              required: { value: true, message: pwmessage.required },
+              minLength: { value: 4, message: pwmessage.letters },
+              maxLength: { value: 4, message: pwmessage.letters },
+              pattern: { value: /^\d+$/, message: pwmessage.number }
+            })}
+          />
         </div>
         <Button text="확인" size="md" type="submit" />
       </div>
+      {errors.password && (
+        <p className={cx('pwpopup-error')}>{errors.password.message}</p>
+      )}
     </form>
   )
 }
