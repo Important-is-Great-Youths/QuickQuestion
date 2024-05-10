e35d3afaefc2e2ef9b1344565d647bc0c613e7e4
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-10 14:20:42 +0900

    QuestionForm에 validation 설정 및 사진 첨부 기능 구현 (#63)
    * refactor: maxLength prop 추가
    
    * design: padding 값 수정
    
    * refactor: Textarea 컴포넌트에 maxLength, ref 추가
    
    * feat: 사진 첨부 및 삭제 기능 구현, validation 설정
    
    * Revert "feat: 사진 첨부 및 삭제 기능 구현, validation 설정"
    
    This reverts commit 0ba8f3ff51be1cf97aed25493f5e9ce65a62f86b.
    
    * Revert "refactor: Textarea 컴포넌트에 maxLength, ref 추가"
    
    This reverts commit f954d3b176a57586c63c351a982b7bc57826615c.
    
    * Revert "design: padding 값 수정"
    
    This reverts commit 6cb36cb4f7778cd33d955392704e61d1d6c6093f.
    
    * Reapply "feat: 사진 첨부 및 삭제 기능 구현, validation 설정"
    
    This reverts commit db7dfb081011316f01b80b0c50b7a29407cac0e0.
    
    * Reapply "design: padding 값 수정"
    
    This reverts commit cd20c739d53b43bd4ec9f61c77278d058497c66f.
    
    * Reapply "refactor: Textarea 컴포넌트에 maxLength, ref 추가"
    
    This reverts commit 88ea8f5cf1acb65aa2231a775e4323e4b0905c8a.

--- src/components/home/QuestionForm/QuestionForm.tsx
+++ src/components/home/QuestionForm/QuestionForm.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
+import Image from 'next/image'
 import { useForm, SubmitHandler, FieldValues } from 'react-hook-form'
 import classNames from 'classnames/bind'
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
 
 import { GetRecipientsList } from '@/types/recipients'
 import { ERROR_MESSAGE, PLACEHOLDER } from '@/constants/formMessage'
 import {
   useGetRecipientsList,
-  usePostRecipientsCreate
+  usePostRecipientsCreate,
+  usePostImageUrlCreate
 } from '@/hooks/useRecipients'
 import Textarea from '@/components/common/Textarea/Textarea'
 import Input from '@/components/common/Input/Input'
@@ -18,20 +21,53 @@ import styles from './QuestionForm.module.scss'
 const cx = classNames.bind(styles)
 
 const QuestionForm = () => {
+  const [imagePreview, setImagePreview] = useState('')
+
   const { data } = useGetRecipientsList(100)
   const { mutate } = usePostRecipientsCreate()
-  console.log(data)
+  const {
+    mutate: getImageUrl,
+    isSuccess: isSuccessGetImageUrl,
+    data: imageUrlData
+  } = usePostImageUrlCreate()
 
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors }
   } = useForm()
+
+  const handleCreateImageUrl = async (
+    value: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (value.target.files) {
+      setImagePreview(URL?.createObjectURL(value.target.files[0]))
+
+      const formData = new FormData()
+      formData.append('image', value.target.files[0])
+      getImageUrl(formData)
+    }
+  }
+
+  const handleResetImage = () => {
+    setValue('backgroundImageURL', '')
+    setImagePreview('')
+  }
+
+  if (isSuccessGetImageUrl) {
+    setValue('backgroundImageURL', imageUrlData.data.url)
+  }
 
   const onSubmit: SubmitHandler<FieldValues> = (value: FieldValues) => {
     value.name = `${value.nickName}/${value.password}/${value.question}`
 
-    const newValue = { ...value, ...{ team: '2-3' } }
+    const { question, nickName, password, backgroundImageSelect, ...values } =
+      value
+
+    const newValue = { ...values, ...{ team: '2-3' } }
+
+    // mutate(newValue)
   }
 
   return (
@@ -41,7 +77,21 @@ const QuestionForm = () => {
           <label className={cx('label')} htmlFor="question">
             질문 내용
           </label>
-          <Textarea id="question" placeholder={PLACEHOLDER.question} />
+          <Textarea
+            id="question"
+            placeholder={PLACEHOLDER.question}
+            maxLength={30}
+            {...register('question', {
+              required: ERROR_MESSAGE.question.required,
+              minLength: { value: 5, message: ERROR_MESSAGE.question.min },
+              maxLength: { value: 30, message: ERROR_MESSAGE.question.max }
+            })}
+          />
+          {errors.question && (
+            <p className={cx('form-field-message')}>
+              {errors.question.message?.toString()}
+            </p>
+          )}
         </div>
         <div className={cx('responsive-flex')}>
           <div className={cx('responsive-flex-item')}>
@@ -53,8 +103,13 @@ const QuestionForm = () => {
               size="responsive"
               type="text"
               placeholder={PLACEHOLDER.nickname}
+              maxLength={4}
               {...register('nickName', {
                 required: ERROR_MESSAGE.nickname.required,
+                maxLength: {
+                  value: 4,
+                  message: ERROR_MESSAGE.nickname.required
+                },
                 validate: (value) =>
                   data.results.some(
                     (user: GetRecipientsList) =>
@@ -79,7 +134,22 @@ const QuestionForm = () => {
               size="responsive"
               type="text"
               placeholder={PLACEHOLDER.password}
-              {...register('password')}
+              maxLength={4}
+              {...register('password', {
+                required: ERROR_MESSAGE.password.required,
+                minLength: {
+                  value: 4,
+                  message: ERROR_MESSAGE.password.letters
+                },
+                maxLength: {
+                  value: 4,
+                  message: ERROR_MESSAGE.password.letters
+                },
+                pattern: {
+                  value: /^\d{4}$/,
+                  message: ERROR_MESSAGE.password.number
+                }
+              })}
             />
             {errors.password && (
               <p className={cx('form-field-message')}>
@@ -96,21 +166,41 @@ const QuestionForm = () => {
         </div>
         <div>
           <label className={cx('label')}>사진 (선택)</label>
-          <div>
-            <label
-              className={cx('form-field-image-label')}
-              htmlFor="backgroundImageSelect"
-            >
-              <Plus className={cx('form-field-image-icon')} />
-            </label>
-            <input
-              className={cx('form-field-image-input')}
-              id="backgroundImageSelect"
-              type="file"
-              // {...register('backgroundImageSelect', {
-              //   onChange: (value) => handleCreateImageUrl(value)
-              // })}
-            />
+          <div className={cx('form-field-image')}>
+            {imagePreview ? (
+              <div
+                className={cx('form-field-image-wrap')}
+                onClick={handleResetImage}
+              >
+                <div className={cx('form-field-image-preview')}>
+                  <Image
+                    className={cx('form-field-image-preview-cover')}
+                    src={imagePreview}
+                    alt="질문 등록 첨부 사진"
+                    fill
+                    sizes="100%"
+                  />
+                </div>
+                <X className={cx('form-field-image-wrap-icon')} />
+              </div>
+            ) : (
+              <>
+                <label
+                  className={cx('form-field-image-label')}
+                  htmlFor="backgroundImageSelect"
+                >
+                  <Plus className={cx('form-field-image-icon')} />
+                </label>
+                <input
+                  className={cx('form-field-image-input')}
+                  id="backgroundImageSelect"
+                  type="file"
+                  {...register('backgroundImageSelect', {
+                    onChange: (value) => handleCreateImageUrl(value)
+                  })}
+                />
+              </>
+            )}
           </div>
         </div>
       </div>
