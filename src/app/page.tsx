36d72f07c9ef84d62e92dd52abbdbb85c6bbda2f
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-12 23:02:30 +0900

    design: 인기 질문 title 스타일 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,28 @@
 
 import classNames from 'classnames/bind'
 
+import CurationCardList from '@/components/home/CurationCardList/CurationCardList'
 import QuestionForm from '@/components/home/QuestionForm/QuestionForm'
 
 import styles from './page.module.scss'
 
 const cx = classNames.bind(styles)
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.main}>
       <div className={cx('container')}>
         <div className={cx('curation-card-list')}>
-          <p>인기 질문</p>
-          <div>card list</div>
+          <p className={cx('curation-card-list-title')}>인기 질문</p>
+          <CurationCardList />
         </div>
         <div>
-          <p className={cx('text')}>빠르게 질문 해보세요!</p>
+          <p className={cx('question-title')}>빠르게 질문 해보세요!</p>
           <QuestionForm />
         </div>
       </div>
     </div>
   )
 }
+
+export default Home
