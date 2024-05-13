0bc74c2e3070d3c8ddc29cab6c7ca8de181c447d
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-13 13:25:08 +0900

    tags : key 설정, useFormProvider 적용

--- src/components/common/Tags/Tags.tsx
+++ src/components/common/Tags/Tags.tsx
@@ -1,8 +1,8 @@
 import classNames from "classnames/bind";
 import styles from "./Tags.module.scss";
-import { useState } from "react";
+import { useFormContext } from 'react-hook-form'
 
-const cx = classNames.bind(styles);
+const cx = classNames.bind(styles)
 
 interface TagsProps {
   isAll?: boolean
@@ -17,17 +17,19 @@ const tagList: Array<'전체' | '학문' | '연예' | '게임' | '기타'> = [
 ]
 
 const Tags = ({ isAll = true }: TagsProps) => {
+  const { register } = useFormContext()
   const filteredTagList = isAll ? tagList : tagList.slice(1)
+
   return (
     <div className={cx('tags')}>
       {filteredTagList.map((tag) => (
-        <div className={cx('tag')}>
+        <div key={tag} className={cx('tag')}>
           <input
             id={tag}
             type="radio"
-            name="Tags"
             value={tag}
             className={cx('radio')}
+            {...register('tags', { required: '분야를 선택해주세요' })}
           />
           <label htmlFor={tag} className={cx('label')}>
             {tag}
