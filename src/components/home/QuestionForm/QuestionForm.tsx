d5f361ecaf179caf6ef9ba39aaf85de45355d295
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-16 14:04:01 +0900

    tag 컴포넌트 사용하는 코드 및 비밀번호 input type이 아이콘에 따라 변경되도록 수정 (#75)
    * chore: 테마 기본값 설정
    
    * refactor: tag 컴포넌트 사용하는 코드 수정
    
    * feat: 비밀번호 input type이 아이콘에 따라 변경되도록 수정

--- src/components/home/QuestionForm/QuestionForm.tsx
+++ src/components/home/QuestionForm/QuestionForm.tsx
@@ -1,11 +1,6 @@
 import { useState } from 'react'
 import Image from 'next/image'
-import {
-  useForm,
-  SubmitHandler,
-  FieldValues,
-  FormProvider
-} from 'react-hook-form'
+import { useForm, SubmitHandler, FieldValues } from 'react-hook-form'
 import classNames from 'classnames/bind'
 import { Plus, X } from 'lucide-react'
 
@@ -66,149 +61,148 @@ const QuestionForm = () => {
 
     const newValue = { ...values, ...{ team: '2-3' } }
 
+    console.log(newValue)
     // mutate(newValue)
   }
 
   return (
-    <FormProvider {...methods}>
-      <form className={cx('form')} onSubmit={handleSubmit(onSubmit)}>
-        <div className={cx('form-field')}>
-          <div>
-            <label className={cx('label')} htmlFor="question">
-              질문 내용
+    <form className={cx('form')} onSubmit={handleSubmit(onSubmit)}>
+      <div className={cx('form-field')}>
+        <div>
+          <label className={cx('label')} htmlFor="question">
+            질문 내용
+          </label>
+          <Textarea
+            id="question"
+            placeholder={PLACEHOLDER.question}
+            maxLength={30}
+            {...register('question', {
+              required: ERROR_MESSAGE.question.required,
+              minLength: { value: 5, message: ERROR_MESSAGE.question.min },
+              maxLength: { value: 30, message: ERROR_MESSAGE.question.max }
+            })}
+          />
+          {errors.question && (
+            <p className={cx('form-field-message')}>
+              {errors.question.message?.toString()}
+            </p>
+          )}
+        </div>
+        <div className={cx('responsive-flex')}>
+          <div className={cx('responsive-flex-item')}>
+            <label className={cx('label')} htmlFor="nickName">
+              닉네임
             </label>
-            <Textarea
-              id="question"
-              placeholder={PLACEHOLDER.question}
-              maxLength={30}
-              {...register('question', {
-                required: ERROR_MESSAGE.question.required,
-                minLength: { value: 5, message: ERROR_MESSAGE.question.min },
-                maxLength: { value: 30, message: ERROR_MESSAGE.question.max }
+            <Input
+              id="nickName"
+              size="responsive"
+              type="text"
+              placeholder={PLACEHOLDER.nickname}
+              maxLength={4}
+              {...register('nickName', {
+                required: ERROR_MESSAGE.nickname.required,
+                maxLength: {
+                  value: 4,
+                  message: ERROR_MESSAGE.nickname.required
+                },
+                validate: (value) =>
+                  data.results.some(
+                    (user: GetRecipientsList) =>
+                      user.name.split('/')[0] === value
+                  )
+                    ? ERROR_MESSAGE.nickname.duplicate
+                    : true
               })}
             />
-            {errors.question && (
+            {errors.nickName && (
               <p className={cx('form-field-message')}>
-                {errors.question.message?.toString()}
+                {errors.nickName.message?.toString()}
               </p>
             )}
           </div>
-          <div className={cx('responsive-flex')}>
-            <div className={cx('responsive-flex-item')}>
-              <label className={cx('label')} htmlFor="nickName">
-                닉네임
-              </label>
-              <Input
-                id="nickName"
-                size="responsive"
-                type="text"
-                placeholder={PLACEHOLDER.nickname}
-                maxLength={4}
-                {...register('nickName', {
-                  required: ERROR_MESSAGE.nickname.required,
-                  maxLength: {
-                    value: 4,
-                    message: ERROR_MESSAGE.nickname.required
-                  },
-                  validate: (value) =>
-                    data.results.some(
-                      (user: GetRecipientsList) =>
-                        user.name.split('/')[0] === value
-                    )
-                      ? ERROR_MESSAGE.nickname.duplicate
-                      : true
-                })}
-              />
-              {errors.nickName && (
-                <p className={cx('form-field-message')}>
-                  {errors.nickName.message?.toString()}
-                </p>
-              )}
-            </div>
-            <div className={cx('responsive-flex-item')}>
-              <label className={cx('label')} htmlFor="password">
-                비밀번호
-              </label>
-              <Input
-                id="password"
-                size="responsive"
-                type="text"
-                placeholder={PLACEHOLDER.password}
-                maxLength={4}
-                {...register('password', {
-                  required: ERROR_MESSAGE.password.required,
-                  minLength: {
-                    value: 4,
-                    message: ERROR_MESSAGE.password.letters
-                  },
-                  maxLength: {
-                    value: 4,
-                    message: ERROR_MESSAGE.password.letters
-                  },
-                  pattern: {
-                    value: /^\d{4}$/,
-                    message: ERROR_MESSAGE.password.number
-                  }
-                })}
-              />
-              {errors.password && (
-                <p className={cx('form-field-message')}>
-                  {errors.password.message?.toString()}
-                </p>
-              )}
-            </div>
-          </div>
-          <div>
-            <label className={cx('label')} htmlFor="category">
-              분야
+          <div className={cx('responsive-flex-item')}>
+            <label className={cx('label')} htmlFor="password">
+              비밀번호
             </label>
-            <Tags isAll={false} />
+            <Input
+              id="password"
+              size="responsive"
+              type="password"
+              placeholder={PLACEHOLDER.password}
+              maxLength={4}
+              {...register('password', {
+                required: ERROR_MESSAGE.password.required,
+                minLength: {
+                  value: 4,
+                  message: ERROR_MESSAGE.password.letters
+                },
+                maxLength: {
+                  value: 4,
+                  message: ERROR_MESSAGE.password.letters
+                },
+                pattern: {
+                  value: /^\d{4}$/,
+                  message: ERROR_MESSAGE.password.number
+                }
+              })}
+            />
+            {errors.password && (
+              <p className={cx('form-field-message')}>
+                {errors.password.message?.toString()}
+              </p>
+            )}
           </div>
-          <div>
-            <label className={cx('label')}>사진 (선택)</label>
-            <div className={cx('form-field-image')}>
-              {imagePreview ? (
-                <div
-                  className={cx('form-field-image-wrap')}
-                  onClick={handleResetImage}
-                >
-                  <div className={cx('form-field-image-preview')}>
-                    <Image
-                      className={cx('form-field-image-preview-cover')}
-                      src={imagePreview}
-                      alt="질문 등록 첨부 사진"
-                      fill
-                      sizes="100%"
-                    />
-                  </div>
-                  <X className={cx('form-field-image-wrap-icon')} />
-                </div>
-              ) : (
-                <>
-                  <label
-                    className={cx('form-field-image-label')}
-                    htmlFor="backgroundImageSelect"
-                  >
-                    <Plus className={cx('form-field-image-icon')} />
-                  </label>
-                  <input
-                    className={cx('form-field-image-input')}
-                    id="backgroundImageSelect"
-                    type="file"
-                    {...register('backgroundImageSelect', {
-                      onChange: (value) => handleCreateImageUrl(value)
-                    })}
+        </div>
+        <div>
+          <label className={cx('label')} htmlFor="category">
+            분야
+          </label>
+          <Tags isAll={false} {...register('backgroundColor')} />
+        </div>
+        <div>
+          <label className={cx('label')}>사진 (선택)</label>
+          <div className={cx('form-field-image')}>
+            {imagePreview ? (
+              <div
+                className={cx('form-field-image-wrap')}
+                onClick={handleResetImage}
+              >
+                <div className={cx('form-field-image-preview')}>
+                  <Image
+                    className={cx('form-field-image-preview-cover')}
+                    src={imagePreview}
+                    alt="질문 등록 첨부 사진"
+                    fill
+                    sizes="100%"
                   />
-                </>
-              )}
-            </div>
+                </div>
+                <X className={cx('form-field-image-wrap-icon')} />
+              </div>
+            ) : (
+              <>
+                <label
+                  className={cx('form-field-image-label')}
+                  htmlFor="backgroundImageSelect"
+                >
+                  <Plus className={cx('form-field-image-icon')} />
+                </label>
+                <input
+                  className={cx('form-field-image-input')}
+                  id="backgroundImageSelect"
+                  type="file"
+                  {...register('backgroundImageSelect', {
+                    onChange: (value) => handleCreateImageUrl(value)
+                  })}
+                />
+              </>
+            )}
           </div>
         </div>
-        <div className={cx('button')}>
-          <Button text="질문하기" size="full" type="submit" />
-        </div>
-      </form>
-    </FormProvider>
+      </div>
+      <div className={cx('button')}>
+        <Button text="질문하기" size="full" type="submit" />
+      </div>
+    </form>
   )
 }
 
