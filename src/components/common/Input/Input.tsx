34aa378fb6c5aa9889859a0508eb9b205226f9ef
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-16 13:14:42 +0900

    feat: 비밀번호 input type이 아이콘에 따라 변경되도록 수정

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './Input.module.scss'
+import { Eye, EyeOff } from 'lucide-react'
 
 const cx = classNames.bind(styles)
 
@@ -14,16 +15,30 @@ interface InputProps {
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ size, type, id, placeholder, maxLength, ...props }, ref) => {
+    const [typeState, setTypeState] = useState(type)
+
+    const handleTypeChange = () => {
+      setTypeState((prev) => (prev === 'password' ? 'text' : 'password'))
+    }
+
     return (
-      <input
-        className={cx('input', `input-size-${size}`)}
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        maxLength={maxLength}
-        ref={ref}
-        {...props}
-      />
+      <div className={cx('container')}>
+        <input
+          className={cx('input', `input-size-${size}`)}
+          type={typeState}
+          id={id}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          ref={ref}
+          {...props}
+        />
+        {type === 'password' &&
+          (typeState === 'text' ? (
+            <Eye className={cx('icon')} onClick={handleTypeChange} />
+          ) : (
+            <EyeOff className={cx('icon')} onClick={handleTypeChange} />
+          ))}
+      </div>
     )
   }
 )
