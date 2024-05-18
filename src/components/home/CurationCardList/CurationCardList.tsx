cbcbf5df63b40fc869145c081fbf0e955af15b2b
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-19 00:47:10 +0900

    feat: 슬라이드 버튼 생성

--- src/components/home/CurationCardList/CurationCardList.tsx
+++ src/components/home/CurationCardList/CurationCardList.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import classNames from 'classnames/bind'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 import { useGetRecipientsList } from '@/hooks/useRecipients'
 import { GetRecipientsList } from '@/types/recipients'
@@ -21,7 +22,7 @@ const CurationCardList = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [transX, setTransX] = useState(0)
   const [cardWidth, setCardWidth] = useState(0)
-  const [visibleCardCount, setVisibleCardCount] = useState(3)
+  const [visibleCardCount, setVisibleCardCount] = useState(0)
 
   const { data: cards } = useGetRecipientsList(100)
 
@@ -29,6 +30,7 @@ const CurationCardList = () => {
 
   const totalCards = 6
   const cardListGap = 16
+  const maxIndex = totalCards - visibleCardCount
 
   const inrange = (v: number, min: number, max: number) => {
     if (v < min) return min
@@ -49,10 +51,20 @@ const CurationCardList = () => {
     const handleResize = () => {
       const currentWindowSize = window.innerWidth
 
-      if (currentWindowSize > 767 && visibleCardCount === 2) {
+      if (currentWindowSize > 767 && visibleCardCount !== 3) {
         setVisibleCardCount(3)
-      } else if (currentWindowSize <= 767 && visibleCardCount === 3) {
+      } else if (
+        currentWindowSize <= 767 &&
+        currentWindowSize > 620 &&
+        visibleCardCount !== 2
+      ) {
         setVisibleCardCount(2)
+      } else if (currentWindowSize <= 619 && visibleCardCount !== 1) {
+        setVisibleCardCount(1)
+      }
+
+      if (currentIndex > maxIndex) {
+        setCurrentIndex(maxIndex)
       }
     }
 
@@ -70,45 +82,60 @@ const CurationCardList = () => {
   }
 
   return (
-    <div className={cx('viewer')}>
-      <ul
-        className={cx('card-list')}
-        style={{
-          transform: `translateX(${-currentIndex * cardWidth + transX}px)`,
-          transition: `transform ${transX ? 0 : cardWidth}ms ease-in-out 0s`
-        }}
-        {...registDragEvent({
-          onDragChange: (deltaX) => {
-            setTransX(inrange(deltaX, -cardWidth, cardWidth))
-          },
-          onDragEnd: (deltaX) => {
-            const maxIndex = cards.length - visibleCardCount
-
-            if (deltaX < -100)
-              setCurrentIndex(inrange(currentIndex + 1, 0, maxIndex))
-            if (deltaX > 100)
-              setCurrentIndex(inrange(currentIndex - 1, 0, maxIndex))
-
-            setTransX(0)
-          }
-        })}
+    <div className={cx('container')}>
+      <button
+        className={cx('button')}
+        onClick={() => setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev))}
       >
-        {cards?.results
-          .sort(
-            (a: GetRecipientsList, b: GetRecipientsList) =>
-              b.reactionCount - a.reactionCount
-          )
-          .map(({ id, name, backgroundColor, messageCount }: CardType) => (
-            <li className={cx('card-list-item')} ref={cardRef} key={id}>
-              <Card
-                id={id}
-                cardTitle={backgroundColor}
-                cardText={name}
-                answerCount={messageCount}
-              />
-            </li>
-          ))}
-      </ul>
+        <ChevronLeft className={cx('button-icon')} />
+      </button>
+      <div className={cx('viewer')}>
+        <ul
+          className={cx('card-list')}
+          style={{
+            transform: `translateX(${-currentIndex * cardWidth + transX}px)`,
+            transition: `transform ${transX ? 0 : cardWidth}ms ease-in-out 0s`
+          }}
+          {...registDragEvent({
+            onDragChange: (deltaX) => {
+              setTransX(inrange(deltaX, -cardWidth, cardWidth))
+            },
+            onDragEnd: (deltaX) => {
+              if (deltaX < -100)
+                setCurrentIndex(inrange(currentIndex + 1, 0, maxIndex))
+              if (deltaX > 100)
+                setCurrentIndex(inrange(currentIndex - 1, 0, maxIndex))
+
+              setTransX(0)
+            }
+          })}
+        >
+          {cards?.results
+            .slice(0, totalCards)
+            .sort(
+              (a: GetRecipientsList, b: GetRecipientsList) =>
+                b.reactionCount - a.reactionCount
+            )
+            .map(({ id, name, backgroundColor, messageCount }: CardType) => (
+              <li className={cx('card-list-item')} ref={cardRef} key={id}>
+                <Card
+                  id={id}
+                  cardTitle={backgroundColor}
+                  cardText={name}
+                  answerCount={messageCount}
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+      <button
+        className={cx('button')}
+        onClick={() =>
+          setCurrentIndex((prev) => (prev < maxIndex ? prev + 1 : prev))
+        }
+      >
+        <ChevronRight className={cx('button-icon')} />
+      </button>
     </div>
   )
 }
