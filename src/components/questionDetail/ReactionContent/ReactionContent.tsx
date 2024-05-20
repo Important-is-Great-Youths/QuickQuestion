f4e106a56041aca4d0b7f7dfc3407f155490ed03
youngeun <00eun1128@gmail.com>
2024-05-20 20:43:22 +0900

    chore : text 내용, 모바일 padding값 수정

--- src/components/questionDetail/ReactionContent/ReactionContent.tsx
+++ src/components/questionDetail/ReactionContent/ReactionContent.tsx
@@ -12,7 +12,7 @@ const ReactionContent = ({ id }: ReactionContentProps) => {
   return (
     <div className={cx('ReactionContent')}>
       <div className={cx('ReactionContent-title')}>
-        질문 또는 답변에 느낌을 이모지로 나타내주세요!
+        질문 또는 답변에 대한 느낌을 이모지로 나타내주세요!
       </div>
       <Reaction id={id}></Reaction>
     </div>
