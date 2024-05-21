5ec27ef33199c08eb9ab8ed95f81a086bdc254ff
mini-chip <75009853+mini-chip@users.noreply.github.com>
2024-05-21 14:45:39 +0900

    QuestionListPage 구현 중 (#94)
    * feat: QuestionList api get
    
    * feat: pagination Questionlist 연동

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,23 +1,29 @@
 import classNames from 'classnames/bind'
 import styles from './Pagination.module.scss'
 import { useState } from 'react'
-import data from './mockdata.json'
+
+import { GetRecipientsList } from '@/types/recipients'
 const cx = classNames.bind(styles)
 
 interface PaginationProps {
   size?: 'lg' | 'md'
+  data: GetRecipientsList
+  limit: number
+  currentPage: number
   totalCount?: number
-  limit?: number
+  onPageChange: (page: number) => void
 }
-//total 데이터 총 갯수, limit 한 페이지에 나타낼 데이터 갯수
+
 export const Pagination = ({
-  totalCount = data.length, //데이터의 총 갯
-  limit = 6, //한 페이지에 나타낼 데이터 수
-  size
+  data,
+  limit,
+  currentPage,
+  totalCount = data.length || 0,
+  size,
+  onPageChange
 }: PaginationProps) => {
   const totalPage = Math.ceil(totalCount / limit)
-  const [currentPage, setCurrentPage] = useState(1) //현재 페이지
-  const [offset, setOffest] = useState(0) //데이터 불러온 갯수
+
   const pageCount = 5 //화면에 보여지는 총 페이지 갯수
 
   let firstNumber = Math.max(1, currentPage - Math.floor(pageCount / 2))
@@ -25,7 +31,6 @@ export const Pagination = ({
 
   const prev = Math.max(1, currentPage - 1) // 이전버튼
   const next = Math.min(totalPage, currentPage + 1) //다음버튼
-
   const buttons = [] //버튼들 배열
   for (let i = firstNumber; i <= lastNumber; i++) {
     buttons.push(
@@ -35,7 +40,7 @@ export const Pagination = ({
         className={cx('button', `button-size-${size}`, {
           'button-active': i === currentPage
         })}
-        onClick={() => setCurrentPage(i)}
+        onClick={() => onPageChange(i)}
       >
         {i}
       </button>
@@ -43,31 +48,23 @@ export const Pagination = ({
   }
 
   return (
-    <div>
-      {data.map(({ id, name }: any) => (
-        <p key={id}>
-          {id} : {name}
-        </p>
-      ))}
+    <div className={cx('pagination')}>
+      <button
+        className={cx('button', `button-size-${size}`)}
+        disabled={currentPage === 1}
+        onClick={() => onPageChange(prev)}
+      >
+        &lt;
+      </button>
+      {buttons}
 
-      <div className={cx('pagination')}>
-        <button
-          className={cx('button', `button-size-${size}`)}
-          disabled={currentPage === 1}
-          onClick={() => setCurrentPage(prev)}
-        >
-          &lt;
-        </button>
-        {buttons}
-
-        <button
-          className={cx('button', `button-size-${size}`)}
-          disabled={currentPage === totalPage}
-          onClick={() => setCurrentPage(next)}
-        >
-          &gt;
-        </button>
-      </div>
+      <button
+        className={cx('button', `button-size-${size}`)}
+        disabled={currentPage === totalPage}
+        onClick={() => onPageChange(next)}
+      >
+        &gt;
+      </button>
     </div>
   )
 }
