b8211c117ca5df696136c60930d8098edd17d41f
youngeun <00eun1128@gmail.com>
2024-05-22 13:10:11 +0900

    feat : QuestionDetailPage 합침, props에 따라 UI 변경

--- src/components/common/AnswerContent/AnswerContent.tsx
+++ src/components/common/AnswerContent/AnswerContent.tsx
@@ -7,7 +7,7 @@ import useDate from '@/hooks/useDate'
 const cx = classNames.bind(styles)
 
 const testData = {
-  src: 'https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  src: undefined,
   nickname: '김답변',
   date: new Date().toISOString(),
   answer: `병아리들은 애기때는 깃털이 아닌 솜털이에요!
