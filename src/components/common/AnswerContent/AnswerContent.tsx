4378c430f4e5c22bb5e6ff951b3fcbfbb126f4e9
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-22 14:00:47 +0900

    refactor : 이벤트 추가  + userType props 추가

--- src/components/common/AnswerContent/AnswerContent.tsx
+++ src/components/common/AnswerContent/AnswerContent.tsx
@@ -5,6 +5,9 @@ import { CheckCircle2, SquarePen, SquareX } from 'lucide-react'
 import useDate from '@/hooks/useDate'
 import PwPopUp from '../PopUp/PwPopUp'
 import { useState } from 'react'
+import { useModal } from '@/contexts/ModalProvider'
+import AlertModal from '../AlertModal/AlertModal'
+import Textarea from '../Textarea/Textarea'
 
 const cx = classNames.bind(styles)
 
@@ -12,11 +15,7 @@ const testData = {
   src: 'https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   nickname: '김답변',
   date: new Date().toISOString(),
-  answer: `병아리들은 애기때는 깃털이 아닌 솜털이에요!
-        부화한지 일주일정도 지나게 되면 노란 솜털이 빠지고하얀 깃털, 갈색 깃털
-        등 다양한 색의 깃털이 나오게 되는거죵.
-        쉽게말해 깃털 색이 노란색에서 하얀색으로 변화하는게 아닌 그냥 솜털
-        자체가 빠지면서 다른색깔인 새 깃털로 바뀐다는겁니다!`
+  answer: `병아리들은 애기때는 깃털이 아닌 솜털이에요! 부화한지 일주일정도 지나게 되면 노란 솜털이 빠지고하얀 깃털, 갈색 깃털 등 다양한 색의 깃털이 나오게 되는거죵. 쉽게말해 깃털 색이 노란색에서 하얀색으로 변화하는게 아닌 그냥 솜털 자체가 빠지면서 다른색깔인 새 깃털로 바뀐다는겁니다!`
 }
 
 interface AnswerContentProps {
@@ -24,6 +23,7 @@ interface AnswerContentProps {
   nickname: string
   date: string
   answer: string
+  userType: 'questioner' | 'respondent' | 'etc'
   onCheck: () => void
 }
 
@@ -31,24 +31,53 @@ const AnswerContent = ({
   profileImage = testData.src,
   nickname = testData.nickname,
   date = testData.date,
+  answer = testData.answer,
+  userType = 'etc',
   onCheck
 }: AnswerContentProps) => {
   const formattedDate = useDate(date)
   const [isPopupOpen, setIsPopupOpen] = useState(false)
   const [popupMode, setPopupMode] = useState('')
+  const modalId = crypto.randomUUID()
+  const { openModal, closeModal } = useModal()
+  const [isTextareaOpen, setIsTextareaOpen] = useState(false)
 
   const handlePopupOpen = () => {
     setIsPopupOpen(!isPopupOpen)
   }
 
-  const onEdit = () => {
+  const handleEditButton = () => {
     handlePopupOpen()
     setPopupMode('edit')
   }
 
-  const onDelete = () => {
+  const handleDeleteButton = () => {
     handlePopupOpen()
     setPopupMode('delete')
+  }
+
+  const handlePopupCheck = () => {
+    if (popupMode === 'edit') {
+      setIsTextareaOpen(true)
+      handlePopupOpen()
+    } else if (popupMode === 'delete') {
+      handleTestModal()
+      handlePopupOpen()
+    }
+  }
+
+  const handleTestModal = () => {
+    openModal(
+      <AlertModal
+        onCancel={() => {
+          closeModal(modalId)
+        }}
+        onDelete={() => {
+          closeModal(modalId)
+        }}
+      />,
+      modalId
+    )
   }
 
   return (
@@ -67,33 +96,44 @@ const AnswerContent = ({
           </div>
           <p>{formattedDate}</p>
         </div>
-        <div className={cx('answercontent-top-buttons')}>
+        {userType === 'respondent' && (
+          <div className={cx('answercontent-top-buttons')}>
+            <button
+              onClick={handleEditButton}
+              className={cx('answercontent-top-buttons-button')}
+            >
+              <SquarePen />
+            </button>
+            <button
+              onClick={handleDeleteButton}
+              className={cx('answercontent-top-buttons-button')}
+            >
+              <SquareX />
+            </button>
+            {isPopupOpen && (
+              <div className={cx('answercontent-top-pwpopup')}>
+                <PwPopUp onCheck={() => handlePopupCheck()} />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      {isTextareaOpen ? (
+        <Textarea id="0" placeholder={answer} maxLength={30} />
+      ) : (
+        <p className={cx('answercontent-middle')}>{answer}</p>
+      )}
+      {userType === 'questioner' && (
+        <div className={cx('answercontent-bottom')}>
           <button
-            onClick={onEdit}
-            className={cx('answercontent-top-buttons-button')}
+            className={cx('answercontent-bottom-check')}
+            onClick={onCheck}
           >
-            <SquarePen />
+            <CheckCircle2 className={cx('answercontent-bottom-check-image')} />
+            <p>채택하기</p>
           </button>
-          <button
-            onClick={onDelete}
-            className={cx('answercontent-top-buttons-button')}
-          >
-            <SquareX />
-          </button>
-          {isPopupOpen && (
-            <div className={cx('answercontent-top-pwpopup')}>
-              <PwPopUp />
-            </div>
-          )}
         </div>
-      </div>
-      <p className={cx('answercontent-middle')}>{testData.answer}</p>
-      <div className={cx('answercontent-bottom')}>
-        <button className={cx('answercontent-bottom-check')} onClick={onCheck}>
-          <CheckCircle2 className={cx('answercontent-bottom-check-image')} />
-          <p>채택하기</p>
-        </button>
-      </div>
+      )}
     </div>
   )
 }
