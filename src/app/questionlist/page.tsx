c740d4c1dc3dd4183a64d072742840c0b20f9992
mini-chip <75009853+mini-chip@users.noreply.github.com>
2024-05-22 12:00:12 +0900

    feat: ListPage 페이지네이션 구현

--- src/app/questionlist/page.tsx
+++ src/app/questionlist/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 
 import classNames from 'classnames/bind'
-
 import styles from './questionlist.module.scss'
 import { useModal } from '@/contexts/ModalProvider'
 import AlertModal from '@/components/common/AlertModal/AlertModal'
+import { useGetRecipientsList } from '@/hooks/useRecipients'
+import { GetRecipientsList } from '@/types/recipients'
+import { useState } from 'react'
+import FormModal from '@/components/common/FormModal/FormModal'
+import { Pagination } from '@/components/common/Pagination/Pagination'
+import Card from '@/components/common/Card/Card'
 
 const cx = classNames.bind(styles)
 
 const QuestionListPage = () => {
   const modalId = crypto.randomUUID()
   const { openModal, closeModal } = useModal()
+  const [selectedQuestion, setSelectedQuestion] = useState<GetRecipientsList>()
+  const [currentPage, setCurrentPage] = useState(1)
+  const limit = 100 // 페이지당 보여줄 항목의 수
+  const viewCount = 6
+  const { data, isLoading, error } = useGetRecipientsList(limit, 0)
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error loading questions</p>
+  }
 
   const handleTestModal = () => {
     openModal(
@@ -25,10 +43,37 @@ const QuestionListPage = () => {
       modalId
     )
   }
+  const totalCount = data.results.length
+  // Math.ceil(현재 페이지 번호 / 한 화면에 보여질 페이지 개수)
+  const pageGroup = Math.ceil(currentPage / 5)
+  const startIndex = (currentPage - 1) * 6
+  // const startIndex = (pageGroup - 1) * 5 + 1
+  const endIndex = startIndex + viewCount
+  const paginatedData = data.results.slice(startIndex, endIndex)
+  console.log(startIndex, endIndex, paginatedData)
+
   return (
     <div className={cx('main')}>
-      QuestionListPage
       <button onClick={handleTestModal}>openmodal</button>
+      <ul>
+        {paginatedData.map((question: GetRecipientsList) => (
+          <Card
+            id={question.id.toString()}
+            cardTitle={'beige'}
+            cardText={question.name}
+            answerCount={question.messageCount}
+          />
+        ))}
+      </ul>
+
+      <Pagination
+        data={data.results}
+        viewCount={viewCount}
+        totalCount={data.results.length}
+        limit={limit}
+        currentPage={currentPage}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </div>
   )
 }
--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,87 +1,90 @@
-import classNames from "classnames/bind";
-import styles from "./Pagination.module.scss";
-import { useState } from "react";
-import data from "./data.json";
-
-const cx = classNames.bind(styles);
+import classNames from 'classnames/bind'
+import styles from './Pagination.module.scss'
+import { GetRecipientsList } from '@/types/recipients'
+const cx = classNames.bind(styles)
 
 interface PaginationProps {
-  size?: "lg" | "md";
-  totalCount?: number;
-  limit?: number;
+  size?: 'lg' | 'md'
+  data: GetRecipientsList
+  limit: number
+  currentPage: number
+  totalCount?: number
+  viewCount: number
+  onPageChange: (page: number) => void
 }
-//total 데이터 총 갯수, limit 한 페이지에 나타낼 데이터 갯수
+
 export const Pagination = ({
-  totalCount = data.length, //데이터의 총 갯
-  limit = 6, //한 페이지에 나타낼 데이터 수
-  size
+  data,
+  limit,
+  currentPage,
+  totalCount,
+  size,
+  viewCount,
+  onPageChange
 }: PaginationProps) => {
-  const totalPage = Math.ceil(totalCount / limit);
-  const [currentPage, setCurrentPage] = useState(1); //현재 페이지
-  const [offset, setOffest] = useState(0); //데이터 불러온 갯수
-  const pageCount = 5; //화면에 보여지는 총 페이지 갯수
+  const totalPage = Math.ceil(totalCount / viewCount)
 
-  let firstNumber = Math.max(1, currentPage - Math.floor(pageCount / 2));
-  let lastNumber = Math.min(totalPage, firstNumber + pageCount - 1);
+  const pageCount = 5 //화면에 보여지는 총 페이지 갯수
+  const currentGroup = Math.ceil(currentPage / pageCount)
+  let firstNumber = Math.max(1, (currentGroup - 1) * pageCount + 1)
+  let lastNumber = Math.min(totalPage, currentGroup * pageCount)
 
-  const prev = Math.max(1, currentPage - 1); // 이전버튼
-  const next = Math.min(totalPage, currentPage + 1); //다음버튼
+  const prevGroup = firstNumber - 1 // 이전 페이지 그룹
+  const nextGroup = lastNumber + 1 // 다음 페이지 그룹
 
-  const buttons = []; //버튼들 배열
+  const prev = Math.max(1, currentPage - 1) // 이전버튼
+  const next = Math.min(totalPage, currentPage + 1) //다음버튼
+
+  const buttons = [] //버튼들 배열
   for (let i = firstNumber; i <= lastNumber; i++) {
     buttons.push(
       <button
         key={i}
         type="button"
-        className={cx("button", `button-size-${size}`, {
-          "button-active": i === currentPage
+        className={cx('button', `button-size-${size}`, {
+          'button-active': i === currentPage
         })}
-        onClick={() => setCurrentPage(i)}
+        onClick={() => onPageChange(i)}
       >
         {i}
       </button>
-    );
+    )
   }
-
+  console.log(currentPage)
+  console.log({ totalPage, currentGroup, firstNumber, lastNumber })
   return (
-    <div>
-      {data.map(({ id, name }) => (
-        <p key={id}>
-          {id} : {name}
-        </p>
-      ))}
-
-      <div className={cx("pagination")}>
+    <div className={cx('pagination')}>
+      <button
+        className={cx('button', `button-size-${size}`)}
+        disabled={currentPage === 1}
+        onClick={() => onPageChange(prev)}
+      >
+        &lt;
+      </button>
+      {currentGroup > 1 && (
         <button
-          className={cx("button", `button-size-${size}`)}
-          disabled={currentPage === 1}
-          onClick={() => setCurrentPage(prev)}
+          className={cx('button', `button-size-${size}`)}
+          onClick={() => onPageChange(prevGroup)}
         >
-          &lt;
+          &laquo;
         </button>
-        {buttons}
-        {/* {Array.from({ length: lastNumber - firstNumber + 1 }, (_, index) => {
-          const pageNumber = firstNumber + index;
-          return (
-            <button
-              key={pageNumber}
-              className={cx("button", `button-size-${size}`, {
-                "button-active": pageNumber === currentPage
-              })}
-              onClick={() => setCurrentPage(pageNumber)}
-            >
-              {pageNumber}
-            </button>
-          );
-        })} */}
+      )}
+      {buttons}
+      {currentGroup < Math.ceil(totalPage / pageCount) && (
         <button
-          className={cx("button", `button-size-${size}`)}
-          disabled={currentPage === totalPage}
-          onClick={() => setCurrentPage(next)}
+          className={cx('button', `button-size-${size}`)}
+          onClick={() => onPageChange(nextGroup)}
         >
-          &gt;
+          &raquo;
         </button>
-      </div>
+      )}
+      <button
+        className={cx('button', `button-size-${size}`)}
+        disabled={currentPage === totalPage}
+        onClick={() => onPageChange(next)}
+      >
+        &gt;
+      </button>
     </div>
-  );
-};
+  )
+}
