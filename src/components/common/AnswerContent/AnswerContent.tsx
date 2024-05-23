a882382433c6cf965a81783196fe2cf58abc4a2d
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-05-24 01:56:01 +0900

    AnswerContent의 비밀번호 입력 팝업, 사용자에 따라서 props로 UI 변경되도록 수정하기 (#102)
    * feat : PwPopup 위치 조정 및 open 이벤트 추가
    
    * refactor : PwPopUp에 onCheck 이벤트 추가
    
    * refactor : 이벤트 추가  + userType props 추가
    
    * refactor : 경로와 userType 수정

--- src/components/common/AnswerContent/AnswerContent.tsx
+++ src/components/common/AnswerContent/AnswerContent.tsx
@@ -3,6 +3,11 @@ import styles from './AnswerContent.module.scss'
 import Image from 'next/image'
 import { CheckCircle2, SquarePen, SquareX } from 'lucide-react'
 import useDate from '@/hooks/useDate'
+import PwPopUp from '../PopUp/PwPopUp'
+import { useState } from 'react'
+import { useModal } from '@/contexts/ModalProvider'
+import AlertModal from '@/components/common/AlertModal/AlertModal'
+import Textarea from '@/components/common/Textarea/Textarea'
 
 const cx = classNames.bind(styles)
 
@@ -10,11 +15,7 @@ const testData = {
   src: 'https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?q=80&w=1180&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   nickname: '김답변',
   date: new Date().toISOString(),
-  answer: `병아리들은 애기때는 깃털이 아닌 솜털이에요!
-        부화한지 일주일정도 지나게 되면 노란 솜털이 빠지고하얀 깃털, 갈색 깃털
-        등 다양한 색의 깃털이 나오게 되는거죵.
-        쉽게말해 깃털 색이 노란색에서 하얀색으로 변화하는게 아닌 그냥 솜털
-        자체가 빠지면서 다른색깔인 새 깃털로 바뀐다는겁니다!`
+  answer: `병아리들은 애기때는 깃털이 아닌 솜털이에요! 부화한지 일주일정도 지나게 되면 노란 솜털이 빠지고하얀 깃털, 갈색 깃털 등 다양한 색의 깃털이 나오게 되는거죵. 쉽게말해 깃털 색이 노란색에서 하얀색으로 변화하는게 아닌 그냥 솜털 자체가 빠지면서 다른색깔인 새 깃털로 바뀐다는겁니다!`
 }
 
 interface AnswerContentProps {
@@ -22,8 +23,7 @@ interface AnswerContentProps {
   nickname: string
   date: string
   answer: string
-  onEdit: () => void
-  onDelete: () => void
+  userType: 'question' | 'answer'
   onCheck: () => void
 }
 
@@ -31,11 +31,55 @@ const AnswerContent = ({
   profileImage = testData.src,
   nickname = testData.nickname,
   date = testData.date,
-  onEdit,
-  onDelete,
+  answer = testData.answer,
+  userType = 'question',
   onCheck
 }: AnswerContentProps) => {
   const formattedDate = useDate(date)
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+  const [popupMode, setPopupMode] = useState('')
+  const modalId = crypto.randomUUID()
+  const { openModal, closeModal } = useModal()
+  const [isTextareaOpen, setIsTextareaOpen] = useState(false)
+
+  const handlePopupOpen = () => {
+    setIsPopupOpen(!isPopupOpen)
+  }
+
+  const handleEditButton = () => {
+    handlePopupOpen()
+    setPopupMode('edit')
+  }
+
+  const handleDeleteButton = () => {
+    handlePopupOpen()
+    setPopupMode('delete')
+  }
+
+  const handlePopupCheck = () => {
+    if (popupMode === 'edit') {
+      setIsTextareaOpen(true)
+      handlePopupOpen()
+    } else if (popupMode === 'delete') {
+      handleTestModal()
+      handlePopupOpen()
+    }
+  }
+
+  const handleTestModal = () => {
+    openModal(
+      <AlertModal
+        onCancel={() => {
+          closeModal(modalId)
+        }}
+        onDelete={() => {
+          closeModal(modalId)
+        }}
+      />,
+      modalId
+    )
+  }
+
   return (
     <div className={cx('answercontent')}>
       <div className={cx('answercontent-top')}>
@@ -52,22 +96,44 @@ const AnswerContent = ({
           </div>
           <p>{formattedDate}</p>
         </div>
-        <div className={cx('answercontent-top-buttons')}>
-          <button onClick={onEdit}>
-            <SquarePen />
-          </button>
-          <button onClick={onDelete}>
-            <SquareX />
+        {userType === 'answer' && (
+          <div className={cx('answercontent-top-buttons')}>
+            <button
+              onClick={handleEditButton}
+              className={cx('answercontent-top-buttons-button')}
+            >
+              <SquarePen />
+            </button>
+            <button
+              onClick={handleDeleteButton}
+              className={cx('answercontent-top-buttons-button')}
+            >
+              <SquareX />
+            </button>
+            {isPopupOpen && (
+              <div className={cx('answercontent-top-pwpopup')}>
+                <PwPopUp onCheck={() => handlePopupCheck()} />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      {isTextareaOpen ? (
+        <Textarea id="0" placeholder={answer} maxLength={30} />
+      ) : (
+        <p className={cx('answercontent-middle')}>{answer}</p>
+      )}
+      {userType === 'question' && (
+        <div className={cx('answercontent-bottom')}>
+          <button
+            className={cx('answercontent-bottom-check')}
+            onClick={onCheck}
+          >
+            <CheckCircle2 className={cx('answercontent-bottom-check-image')} />
+            <p>채택하기</p>
           </button>
         </div>
-      </div>
-      <p className={cx('answercontent-middle')}>{testData.answer}</p>
-      <div className={cx('answercontent-bottom')}>
-        <button className={cx('answercontent-bottom-check')} onClick={onCheck}>
-          <CheckCircle2 className={cx('answercontent-bottom-check-image')} />
-          <p>채택하기</p>
-        </button>
-      </div>
+      )}
     </div>
   )
 }
