d412dffa5ffc4b080f6cab4dc7428a24eb830a4f
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-05-31 19:37:08 +0900

    feat: 채택 수정 시 기존 텍스트를 기본값으로 설정

--- src/components/common/Textarea/Textarea.tsx
+++ src/components/common/Textarea/Textarea.tsx
@@ -8,17 +8,19 @@ const cx = classNames.bind(styles)
 interface TextareaProps {
   id: string
   placeholder: string
+  defaultValue?: string
   maxLength?: number
   onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ id, placeholder, maxLength, onChange, ...props }, ref) => {
+  ({ id, placeholder, defaultValue, maxLength, onChange, ...props }, ref) => {
     return (
       <textarea
         className={cx('textarea')}
         id={id}
         placeholder={placeholder}
+        defaultValue={defaultValue}
         ref={ref}
         maxLength={maxLength}
         onChange={onChange}
