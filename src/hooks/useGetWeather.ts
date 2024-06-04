ea6d00afe3827928f58b8739b985fea37b7f5b97
ymj0828 <96277798+ymj0828@users.noreply.github.com>
2024-06-04 21:05:30 +0900

    변환된 위치값, 현재 날짜를 데이터 요청 url에 넣기 및 질문 없을 때 렌더링 될 요소 추가 (#161)
    * feat: 현재 날짜, 현재 시각, 경도, 위도를 받아와서 url에 입력
    
    * refactor: 질문 리스트에 질문이 없을 때 렌더링 될 요소 추가

--- src/hooks/useGetWeather.ts
+++ src/hooks/useGetWeather.ts
@@ -3,12 +3,20 @@ import { useTheme } from 'next-themes'
 import { useQuery } from '@tanstack/react-query'
 
 import { getWeather } from '@/apis/getWeather'
+import { useBaseDate, useBaseTime } from '@/hooks/useDateAndTime'
+import { useLonLatToXY } from '@/hooks/useLonLatToXY'
 
 const useGetWeather = () => {
   const { setTheme } = useTheme()
+
+  const dateSlot = useBaseDate()
+  const timeSlot = useBaseTime()
+  const { x, y } = useLonLatToXY(126.980008333333, 37.5635694444444) // 임시값
+
   const { data } = useQuery({
     queryKey: ['weather'],
-    queryFn: () => getWeather()
+    queryFn: () => getWeather(dateSlot, timeSlot, x, y),
+    enabled: !!timeSlot
   })
 
   const obsrValueCode = data?.data.response.body.items.item[0].obsrValue
